44c5af082a33a5cba2d377c5aa79c380a3d70e37
Guirdo <produccionesaldo@gmail.com>
2019-06-03 11:53:14 -0500

    Arreglando error 500 intento 1

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -120,6 +120,7 @@ return [
     */
 
     'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'itch-papeleria-AJL'),
 
     'cipher' => 'AES-256-CBC',
 
